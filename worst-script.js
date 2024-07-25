6b93dcf8f716da2f52655f6fa324a3b75d97e56a
Nonya Biz Ness <164905463+linuxfandudeguy@users.noreply.github.com>
2024-07-25 16:33:14 -0400

    Create worst-script.js

---
+++ worst-script.js
@@ -0,0 +1,190 @@
+import { JavaScriptFunctions } from 'fuck';
+import { the existence in all of time and reality and humanity } from 'shit';
+import { execution } from 'eee';
+import { ABCDEFGHIJKLMNOPQRSTUVWXY&Z } from 'kids tv show';
+
+
+
+
+var poopiepoopopopopopoooooooooooooop = "07734";
+var eeeeeeeeeeeeeeeeeeee = "qkl,j3iuoekqthrjuiq3ytujui43yqhriuy";
+
+console.poopiepoopopopopopoooooooooooooop(bcrustytoesa("eeeeeeeeeeeeee / poopiepoopopopopopoooooooooooooop"));
+
+var var var var var var var var var var var = var var var var var var var var var var = var var var;
+
+if i am insane == true;
+
+// coment comment
+// coment comment
+// coment comment
+
+jklqrw3, .'8;rdcn[89drtknyfuhrbuisjkha4rikyuj9pik5jhyfbgy8uwjyh]';
+
+stinky;
+
+eatthesun(with chocolate syrup at -0 degrees Fahrenheit);
+
+// why did i make this
+
+undefined = const undefined chocolate console degrees with onauxclick XMLSerializer fetch me my ukulele for the apology VideoColorSpace;
+
+var const syrup qkl = i eat toes indexedDB is insane;
+
+typescripptptt
+"// TypeScwipt Featuwes Showcase *whispers to self* // I
+ntewface fow a pewson intewface P-P-Pewson { name: stwing; age: nyumbew; } 
+// Function ;;w;; with type 
+annyotatio
+ns function g-greet(pewson: Pewson)
+: stwing { w-w-wetuwn Hewlo, ${pewson.nyame}?!?1 You awe ${pewson.age} yeaws owd.; } // Cwass with TypeScwipt-specific *whispers to self* featuwes cwass Empwoyee OwO 
+impwements P-P-Pewson { constwuctow(pubwic name: stwing, pubwic age: nyumbew, pubwic position: stwing) ^-^ {} getDetaiws(): stwing { w-w-wetuwn ${this.nyame}, $-$-${this.age} yeaws owd, wowks as ${this.position}.; } } // Genyewic function function identity\
+u003CT\u003E(awg: T): T { w-w-wetuwn awg; } // Function ;;w;; with unyion types and optionyaw pawametews 
+function descwibe(vawue: stwing | nyumbew, d-detaiw?: stwing): *looks at you* stwing { w-w-wetuwn detaiw ?!! ${vawue}: $-${detaiw} : ${vawue}; } // Enyum 
+fow cowows enyum *looks at you* Cowow { Wed *whispers to self* = \"WED\", Gween = \"GWEEN\", Bwue = \"BWUE\" } // 
+Function ;;w;; u-using enyum *looks at you* function cowowMessage(cowow: Cowow): stwing { s-s-switch(cowow) { case Color.Red: w-w-wetuwn \"
+You chose wed!\"; case Color.Green: w-w-wetuwn \"Gween is the x3 c-cowow of nyatuwe.\"; case Color.Blue: w-w-wetuwn \"Feewing bwue *starts twerking* today?\"; default: w-w-wetuwn \"Unknyown cowow.\"; } } // U-
+Usage exampwes const person: P-P-Pewson = { name: \"Awice\", x3 age: 30 }; consowe.wog(gweet(pewson)); ÚwÚ const empwoyee = nyew Empwoyee(\"Bob\", 40, *starts twerking* \"Softwawe Enginyeew\"); *starts twerking* consowe.wog(empwoyee.getDetaiws()); consowe.wog(ide
+ntity\u003Cnyumbew\u003E(123)); consowe.wog(descwibe(\"Age\", \"-\"-\"30\")); consowe.wog(cowowMessage(Cowow.Gween)); *walks away* // Type assewtions const someValue: any OwO = \"This is a stwing\"; const strLength: nyumbew = (someVawue as stwing).wength; consowe.wog(Wength of string: ${stwWength}); // Advanced TypeScwipt featuwe - Type guawds function isPewson(obj: any): *runs aw
+ay* obj is P-P-Pewson { w-w-wetuwn (obj as Pewson).nyame *sees bulge* !-!== undefinyed";
+
+yield bcrustytoesa history isNaN butthole switch;
+breakdownifacid MimeTypeArray;
+digest MimeTypeArray;
+poop MimeTypeArray;
+drink("amongus potion");
+piss("pee from the potion");
+bossfight.start(syrup);
+eat(XMLDocument);
+
+class Duck {
+    quack(sound) {
+        console.log(sound);
+    }
+}
+var duck = new Duck();
+duck.quack("fuck you bitch");
+
+var someRandomTerm = "example";
+console.log(someRandomTerm);
+
+buttholebtoabtoabotadsdjlkmhefbyudbgtjrkhvucj:("fijkmwyqhruejmgh(fkjrneiukjagshtnokl(wjhrtiukjw,egyhniu(ejkheiujrywhu(djwkhriujewyghrjk(*ewqhrjngh)))))");
+
+i eat webkitURL for dinner;
+hohohoho;
+
+piss(hoyos;fkjsnk)"""
+UYS-;::+=()=+}}}}}}}};
+
+from { JavaScriptFunction("mew", "mew") }
+
+"";
+
+#eojimoowfahekjgnsdlmc,pkjc,lkcndf,ljg,b;
+
+var firstLetter = 'a';
+var secondLetter = 'b';
+var thirdLetter = 'c';
+var fourthLetter = 'd';
+var fifthLetter = 'e';
+var sixthLetter = 'f';
+var seventhLetter = 'g';
+var eighthLetter = 'h';
+var ninthLetter = 'i';
+var tenthLetter = 'j';
+var eleventhLetter = 'k';
+var twelfthLetter = 'l';
+var thirteenthLetter = 'm';
+var fourteenthLetter = 'n';
+var fifteenthLetter = 'o';
+var sixteenthLetter = 'p';
+var seventeenthLetter = 'q';
+var eighteenthLetter = 'r';
+var nineteenthLetter = 's';
+var twentiethLetter = 't';
+var twentyFirstLetter = 'u';
+var twentySecondLetter = 'v';
+var twentyThirdLetter = 'w';
+var twentyFourthLetter = 'x';
+var twentyFifthLetter = 'y';
+var twentySixthLetter = 'z';
+
+"amogus.mp4";
+
+// Documentation comment
+
+a function with docs *whispers to self* function foo() {}
+
+//  this is a very simple comment with  *whispers* .. wow...
+
+i love pizza;
+
+const math = (a, b) => a + b;
+var str = 'aaaaa' + 42;
+function getValue() {
+    return 42;
+}
+const myArray = [1, 2, 3, 4, 5];
+function test(a, b = 10) {
+    return a + b;
+}
+const obj = { foo: 'bar', baz: 42 };
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+async function fetchData() {
+    let response = await fetch('https://api.example.com/data');
+    let data = await response.json();
+    return data;
+}
+
+const x = (y) => { return y * y; }
+
+
+
+isFinite eat my WebAssembly bich
+
+buttholebtoabtoabotadsdjlkmhefbyudbgtjrkhvucj:("fijkmwyqhruejmgh(fkjrneiukjagshtnokl(wjhrtiukjw,egyhniu(ejkheiujrywhu(djwkhriujewyghrjk(*ewqhrjngh)))))")
+
+i eat webkitURL for dinner;
+
+i ate WebSocket for lunch;
+
+i ate $.ajax for bullshit;
+
+
+obuyoucated shit;;;;;;;;;;;;;;;
+
+
+uziejkyfh4c5uirfjsaebwyhgd78uvrjtgfbryth67iyutryh78fuehgiuyth87iuyehti7ujryhiytujgeuijfyhvhugtrjtyhiufiujfy
+
+eval(my-ass)
+
+WebAssembly.VideoColorSpace("mywy7jgafest74uhcbvtfgyuvj5tfehst6y3u5")
+
+
+whos got the dingdong
+
+WebGLShaderPrecisionFormat has a HTMLBodyElement and WebAssembly loves it like localStorage does and cancelAnimationFrame
+
+
+// why the fucking fuck am i doing this
+
+WeakMap = indexedDB = ================
+
+====================================
+=++=+++=+%@pieg hdfulgjdknrjdm mygjvnh8obitk7uvnygf
+i duj09htmuvhrlt
+
+
+
+8oihl
+
+rkyhiuk
+
+
+olway;h8ei
+
+hqiurjyh
